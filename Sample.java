2bdf8d123851d1e053ec906948594d8530b39a8d
Arjun Govindaraj <govindarajarjun9@gmail.com>
2026-08-13 08:50:44 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,8 +1,8 @@
 public class Sample {
 
     public static void main(String[] args) {
-        System.out.println("Hello Arjun!!!!   How are you  /.......");
-        System.out.println("Hello..");
+        System.out.println("Hello Arjun!!!!   How are you ...");
+        
     }
 
 }
